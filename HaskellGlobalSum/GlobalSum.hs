08616c7ca0bdc9074534b1174a4b190d11718f02
Miles <milescenabeverly@aol.com>
2020-04-06 13:34:33 -0600

    made some more notes

--- HaskellGlobalSum/GlobalSum.hs
+++ HaskellGlobalSum/GlobalSum.hs
@@ -22,3 +22,12 @@ globalSum arr
             lower = globalSum [arr !! i | i <- [0..len `div` 2]]
             upper = globalSum [arr !! i | i <- [len `div` 2 + 1..len]]
             len = length arr - 1
+
+parGlobalSumTest arr
+    | length arr == 0 = 0
+    | length arr == 1 = arr !! 0
+    | otherwise = upper `par` (lower `pseq` (lower + upper))
+        where
+            lower = parGlobalSum (fst split)
+            upper = parGlobalSum (snd split)
+            split = splitAt (length arr `div` 2) arr
