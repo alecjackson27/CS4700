534191d8efd8758b24303bbf555fabfea1edbc1b
Miles <milescenabeverly@aol.com>
2020-04-09 22:23:28 -0600

    cleaned up my code a bit more

--- HaskellGlobalSum/SumMain.hs
+++ HaskellGlobalSum/SumMain.hs
@@ -1,26 +1,31 @@
 module Main where
 
-import Data.Time.Clock (diffUTCTime, getCurrentTime)
 import System.Environment (getArgs)
 import System.Random (StdGen, getStdGen, randoms)
+import Data.Time.Clock (diffUTCTime, getCurrentTime)
 import Control.Parallel (par, pseq)
 import GlobalSum
 
-
+-- Function to force entire generated list to be evaluated immediately, rather than
+-- through lazy evaluation. This will the main script to only measure the algorithm
+-- time, rather than the time to evaluate the list and run the algorithm.
 force :: [a] -> ()
 force xs = go xs `pseq` ()
     where go (_:xs) = go xs
           go [] = 1
 
+-- Function to generate random numbers to be summed
 genRands :: Int -> StdGen -> [Int]
 genRands n generator = force result `seq` result
                 where
-                    result =take n (randoms generator)
+                    result = take n (randoms generator)
 
+-- main script testing the function's runtime.
 main = do
     args <- getArgs
     let count = read (head args)
-    lst <- genRands count `fmap` getStdGen
+    generator <- getStdGen
+    let lst = genRands count generator
     putStrLn $ "Computing global sum of " ++ show count ++ " numbers..."
     start <- getCurrentTime
     let summation = parGlobalSum lst
